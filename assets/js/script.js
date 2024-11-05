1950fb38194b2d4442917b33c811cbcc9615a1c5
Meryem Essalhi <meryem.essalhi@gmail.com>
2024-11-05 16:32:31 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// functions needed : 
+// runGame()
+// checkAnswer()
+// calculateCorrectAnswer()
+// incrementScore
+// incrementWrongAnswer()
+// dispalyAdditionQuestion()
+// dispalySubtractQuestion()
+// displayMultiplyQuestion()
+function runGame(){
+
+}
+function checkAnswer(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function dispalyAdditionQuestion(){
+
+}
+function dispalySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+    
+}
