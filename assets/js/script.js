48d7bef73ea8e9a805552ebaee28bbefa1755b9d
Meryem Essalhi <meryem.essalhi@gmail.com>
2024-11-06 11:46:48 +0000

    Add code to increment score and incorrect answers tally

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,9 +48,11 @@ let calculatedAnswer=calculateCorrectAnswer();
 let isCorrect=userAnswer===calculatedAnswer[0];
 if(isCorrect){
     alert("Hey! You got it right! :D");
+    incrementScore();
 }
 else{
     alert(`Awwwww...you answered ${userAnswer} . The correct answer was ${calculatedAnswer[0]}`);
+    incrementWrongAnswer();
 }
 runGame(calculatedAnswer[1]);
 }
@@ -69,11 +71,18 @@ if(op==="+"){
     throw`Unimplemented operator ${op}.Aborting!`;
 }
 }
+/**Gets the current score from the DOM and increments it by 1 */
 function incrementScore(){
-
+   
+let oldScore=parseInt(document.getElementById("score").innerText);
+document.getElementById("score").innerText=++oldScore;
+  
 }
+/**Gets the current tally of incorrect answers from the DOM and increments it by 1 */
 function incrementWrongAnswer(){
-
+    let oldScore=parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText=++oldScore;
+      
 }
 function dispalyAdditionQuestion(operand1,operand2){
 document.getElementById('operand1').textContent=operand1;
